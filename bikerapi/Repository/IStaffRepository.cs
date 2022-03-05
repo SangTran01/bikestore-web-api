b1238a07900afcb7111d8a5c08a95fb768eba2ae
sangtran <stran8089@gmail.com>
2022-03-05 14:43:52 -0500

    initial commit

---
+++ bikerapi/Repository/IStaffRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using bikerapi.Models;
+
+namespace bikerapi.Repository
+{
+    public interface IStaffRepository
+    {
+        public Task<IEnumerable<Staff>> GetStaffs();
+    }
+}
---
+++ bikerapi/Repository/StaffRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using bikerapi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace bikerapi.Repository
+{
+    public class StaffRepository: IStaffRepository
+    {
+        private readonly BikeStoresContext _context;
+        public StaffRepository(BikeStoresContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Staff>> GetStaffs()
+        {
+            return await _context.Staffs.ToListAsync();
+        }
+    }
+}
