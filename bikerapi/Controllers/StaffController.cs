e77bea51939ce15b0f887cc2aafca19f15fa61c0
sangtran <stran8089@gmail.com>
2022-03-05 14:30:22 -0500

    * .gitignore: Initial Commit. Example of .net web api made in Mac   Visual studio connected to a docker sql server.
    * bikerapi.sln:
    * Program.cs:
    * Startup.cs:
    * bikerapi.csproj:
    * appsettings.json:
    * WeatherForecast.cs:
    * appsettings.Development.json:
    * StaffController.cs:
    * launchSettings.json:
    * WeatherForecastController.cs:

---
+++ bikerapi/Controllers/StaffController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using bikerapi.Models;
+using bikerapi.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace bikerapi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StaffController : ControllerBase
+    {
+        private readonly IStaffRepository _staffRepository;
+        public StaffController(IStaffRepository staffRepository) {
+            _staffRepository = staffRepository;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Staff>> GetStaffs()
+        {
+            var res = await _staffRepository.GetStaffs();
+            return res;
+        }
+    }
+}
